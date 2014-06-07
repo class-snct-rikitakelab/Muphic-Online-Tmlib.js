5fced617b04fbd640df5d7a4818d59f2a5f00c22
yoshiaki rikitake <yoshiaki@sendai-nct.ac.jp>
2014-06-07 14:18:42 +0900

    MenuSceneの実装についてrefactoring

--- script/Menu/MenuScene.js
+++ script/Menu/MenuScene.js
@@ -1,8 +1,6 @@
-
 tm.define("MenuScene", {
-	storyButtonArray: [],
-	musicButtonArray: [],
-
+	storyButtons: [],
+	musicButtons: [],
 
 	superClass : "tm.app.Scene",
 	init : function() {
@@ -13,16 +11,15 @@ tm.define("MenuScene", {
 		this.superInit();
 		this.background = MenuBackgroundManager(this);
 
+		this.storyButtons.push(new StoryButtonManager(this,storyButtonLocationX.no1,story_height,0));
+		this.storyButtons.push(new StoryButtonManager(this,storyButtonLocationX.no2,story_height,1));
+		this.storyButtons.push(new StoryButtonManager(this,storyButtonLocationX.no3,story_height,2));
+		this.storyButtons.push(new StoryButtonManager(this,storyButtonLocationX.no4,story_height,3));
 
-		this.storyButtonArray.push(new StoryButtonManager(this,storyButtonLocationX.no1,story_height,0));
-		this.storyButtonArray.push(new StoryButtonManager(this,storyButtonLocationX.no2,story_height,1));
-		this.storyButtonArray.push(new StoryButtonManager(this,storyButtonLocationX.no3,story_height,2));
-		this.storyButtonArray.push(new StoryButtonManager(this,storyButtonLocationX.no4,story_height,3));
-
-		this.musicButtonArray[0] = new MusicButtonManager(this,musicButtonLocationX.no1,music_height,0);
-		this.musicButtonArray[1] = new MusicButtonManager(this,musicButtonLocationX.no2,music_height,1);
-		this.musicButtonArray[2] = new MusicButtonManager(this,musicButtonLocationX.no3,music_height,2);
-		this.musicButtonArray[3] = new MusicButtonManager(this,musicButtonLocationX.no4,music_height,3);
+		this.musicButtons.push(new MusicButtonManager(this,musicButtonLocationX.no1,music_height,0));
+		this.musicButtons.push(new MusicButtonManager(this,musicButtonLocationX.no2,music_height,1));
+		this.musicButtons.push(new MusicButtonManager(this,musicButtonLocationX.no3,music_height,2));
+		this.musicButtons.push(new MusicButtonManager(this,musicButtonLocationX.no4,music_height,3));
 
 		this.introDialog = MenuIntroDialog(this);
 		this.yesButton = MenuYesButton(this);
@@ -34,17 +31,11 @@ tm.define("MenuScene", {
 	},
 
 	activateStoryButtons: function() {
-		this.storyButtonArray[0].setEnableState();
-		this.storyButtonArray[1].setEnableState();
-		this.storyButtonArray[2].setEnableState();
-		this.storyButtonArray[3].setEnableState();
+		this.storyButtons.forEach(function(btn) { btn.setEnableState();} );
 	},
 
 	activateMusicButtons: function() {
-		this.musicButtonArray[0].setEnableState();
-		this.musicButtonArray[1].setEnableState();
-		this.musicButtonArray[2].setEnableState();
-		this.musicButtonArray[3].setEnableState();		
+		this.musicButtons.forEach(function(btn) { btn.setEnableState();} );
 	}
 
 })
