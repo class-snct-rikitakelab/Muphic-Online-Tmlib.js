6f2b2146b73ca13825935b28275b226a8644d2f7
polyome <s39339@sendai-nct.jp>
2014-10-29 15:10:28 +0900

    miss update

---
+++ app/script/Music/GuitarButton/GuitarButtonView.js
@@ -0,0 +1,17 @@
+tm.define("GuitarButtonView", {
+	superClass : "MVCViewButton",
+	init : function(manager) {
+		var image = "music_pianoButton";
+		var width = 106;
+		var height = 72;
+		var x = 910;
+		var y = 315;
+		this.superInit(image, width, height, x, y, manager);
+	},
+	showNoteFocus : function() {
+		this.getController().showNoteFocus();
+	},
+	reset : function() {
+		this.getController().reset();
+	},
+})
