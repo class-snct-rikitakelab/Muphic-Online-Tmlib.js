7843344ab22ebb91f6b0f6df61152ea1c4efaf21
polyome <s39339@sendai-nct.jp>
2015-01-22 17:33:20 +0900

    changing switching sound

---
+++ app/script/Music/ClarinetButton/ClarinetButtonManager.js
@@ -0,0 +1,45 @@
+tm.define("ClarinetButtonManager", {
+	superClass : "MVCManagerButton",
+	init : function(scene) {
+		this.superInit();
+		this.model = ClarinetButtonModel(this, scene);
+		this.view = ClarinetButtonView(this);
+		this.controller = ClarinetButtonController(this);
+		this.initModel();
+		this.initView();
+		this.initController();
+		this.addToScene();
+		this.setDarkState();
+	},
+	showNoteFocus : function() {
+		if(this.getRemoveNoteButton().getIsAvailable()) {
+			this.getRemoveNoteButton().reset();
+		}
+
+		if(this.getPianoButton().getIsAvailable()) {
+			this.getPianoButton().reset(); 
+		}
+		this.setPushState();
+		this.getHumen().getHumen().setShowNoteFocusState();
+	},
+	reset : function() {
+		this.setEnableState();
+		this.getHumen().getHumen().setDisableState();
+	},
+	getIsAvailable : function() {
+		return this.getModel().getIsAvailable();
+	},
+	setEnableState : function() {
+		this.getView().setState(ClarinetButtonEnableState(this.getView()));
+	},
+	setPushState : function() {
+		this.getView().setState(ClarinetButtonPushState(this.getView()));
+	},
+
+	getHumen : function() { return this.humen; },
+	setHumen : function(humen) { this.humen = humen; },
+	getPianoButton : function() { return this.pianoButton; },
+	setPianoButton : function(pianoButton) { this.pianoButton = pianoButton; },
+	getRemoveNoteButton : function() { return this.removeNoteButton; },
+	setRemoveNoteButton : function(removeNoteButton) { this.removeNoteButton = removeNoteButton; },
+})
