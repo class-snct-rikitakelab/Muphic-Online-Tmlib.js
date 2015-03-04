94d6d26c21e142072097910283ddda1679c92585
polyome <a1402030@sendai-nct.jp>
2015-03-04 20:05:40 +0900

    change image

--- app/script/Music/MenuButton/MenuButtonView.js
+++ app/script/Music/MenuButton/MenuButtonView.js
@@ -2,8 +2,8 @@ tm.define("MenuButtonView", {
 	superClass : "MVCViewButton",
 	init : function(manager,x,y) {
 		var image = "music_menuButton";
-		var width = 106;
-		var height = 72;
+		var width = 64;
+		var height = 58;
 		var x = 30;
 		var y = 30;
 		this.superInit(image, width, height, x, y, manager);
